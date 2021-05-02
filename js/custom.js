648fe2bf02a4ac0405ad1cae29b8abec0297cfff
easin91 <74418735+easin91@users.noreply.github.com>
2021-05-02 14:48:00 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+var btn = document.querySelectorAll('.tab-btn');
+var tab = document.querySelectorAll('.tab');
+
+function reset(){
+
+	for (var i = 0; i < tab.length; i++) {
+		tab[i].style.display='none';
+		btn[i].style.backgroundColor = '#222';
+	}
+}
+
+function tabOne(){
+	reset();
+	tab[0].style.display='block';
+	btn[0].style.backgroundColor = '#08c';
+
+}
+tabOne();
+
+for(let a = 0; a < btn.length; a++){
+	btn[a].addEventListener('click', function(){
+		reset();
+		tab[a].style.display='block';
+		btn[a].style.backgroundColor = '#08c';
+	});
+}
